65cd1e31757c2b54f80f50aec71aea8814bb80a3
ychyrvon <ychyrvon@erecruit.com>
2019-05-15 12:32:54 +0300

    fix HW1

---
+++ HW1/HW1/HW1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HW1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square1 = new Square();
+            Circle circle1 = new Circle();
+
+            Console.WriteLine($"Area of a Square: {square1.AreaOfSquare()} \nArea of a Circle: {Math.Round(circle1.AreaOfCircle(), 2)}");
+            Сalculation calc = new Сalculation();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW1/HW1/HW1/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Square
+    {
+        public int Side;
+        public Square()
+        {
+            Side = new Сalculation().EnterValue("Enter the length of the side of the Square: ");
+        }
+
+        public double AreaOfSquare()
+        {
+            return Math.Pow(Side, 2);
+        }
+    }
+}
