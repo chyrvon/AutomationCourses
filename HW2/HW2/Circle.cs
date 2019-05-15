85bca1d8792ed893f1b354860b30900fdd8710ce
ychyrvon <ychyrvon@erecruit.com>
2019-05-15 15:42:08 +0300

    fixed

---
+++ HW2/HW2/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class Circle
+    {
+        public int Radius;
+        public Circle()
+        {
+            Radius = new Сalculation().EnterValue("Enter the radius of the Circle: ");
+        }
+
+        public double AreaOfCircle()
+        {
+            return Constants.Pi * Math.Pow(Radius, 2);
+        }
+    }
+}
--- HW2/HW2/Program.cs
+++ HW2/HW2/Program.cs
@@ -1,75 +1,35 @@
 ﻿using System;
+using HW2;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CustomMethods
+namespace HW2
 {
-    static class Constants
-    {
-        public const double Pi = 3.14159;
-    }
-    
-    public static class Сalculation
-    {
-        public static int EnterValue(string StrMessage)
-        {
-            int Value;
-            Console.Write(StrMessage);
-        RetryEnterLengthSide:
-            try
-            {
-                Value = Convert.ToInt32(Console.ReadLine());
-                if (Value <= 0) throw new NullReferenceException();
-            }
-            catch
-            {
-                Console.Write("Введено не валидное значение, введите целое число больше 0: ");
-                goto RetryEnterLengthSide;
-            }
-            return Value;
-        }
-        public static double CompareS(int LenSide, int RCircle)
-        {
-            double ValueReturns = 0;
-            if (RCircle >= LenSide/2) return ValueReturns = 1;
-            if (Math.Sqrt(LenSide.SSquare()) > 2 * Math.Sqrt(RCircle.SSquare()/ Constants.Pi)) return ValueReturns = 2;
-            return ValueReturns;
-        }
-        public static double SSquare(this int LenSide)
-        {
-            return Math.Pow(LenSide, 2);
-        }    
-        public static double SCircle(this int RCircle)
-        {
-            return Constants.Pi * Math.Pow(RCircle, 2);
-        }
-    }
-}
-
-namespace HW_Task2
-{
-    using CustomMethods;
     class Program
     {
         static void Main(string[] args)
         {
-            int LengthSide = Сalculation.EnterValue("Введите длинну стороны квадрата: ");
-            int Radius = Сalculation.EnterValue("Введите радиус круга: ");
-            Console.WriteLine($"Площадь квадрата: {LengthSide.SSquare()} Площадь круга: {Radius.SCircle()}");
-            switch (Сalculation.CompareS(LengthSide, Radius))
+            Square square1 = new Square();
+            Circle circle1 = new Circle();
+
+            Console.WriteLine($"Area of a Square: {square1.AreaOfSquare()} \nArea of a Circle: {Math.Round(circle1.AreaOfCircle(), 2)}");
+            Сalculation calc = new Сalculation();
+
+            switch (calc.CompareArea(square1.Side, circle1.Radius))
             {
                 case 0:
-                    Console.Write("Квадрат не вписан в круг.\n ValueReturns = 0");
+                    Console.Write("Figures intersect");
                     break;
                 case 1:
-                    Console.Write("Круг расположен в квадрате");
+                    Console.Write("The circle is fit in the square");
                     break;
                 case 2:
-                    Console.Write("Квадрат расположен в круге");
+                    Console.Write("The square is fit in a circle");
                     break;
             }
             Console.ReadKey();
         }
     }
 }
-
-
-    
